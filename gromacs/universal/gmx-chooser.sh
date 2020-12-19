f64eadd4f231739264370de2b58ef6a712c1aeca
Vladimír Višňovský <467814@mail.muni.cz>
2020-12-19 20:22:19 +0000

    double version fix

--- gromacs/universal/gmx-chooser.sh
+++ gromacs/universal/gmx-chooser.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 FLAGS=`cat /proc/cpuinfo | grep ^flags | head -1`
 if echo $FLAGS | grep " avx512f " > /dev/null; then
-        PATH=$PATH:/gromacs/AVX_512_ts/bin
+        PATH=$PATH:/gromacs/AVX_512/bin
 elif echo $FLAGS | grep " avx2 " > /dev/null; then
         PATH=$PATH:/gromacs/AVX2_256/bin
-elif echo $FLAGS | grep " avx " > /dev/null; then
-        PATH=$PATH:/gromacs/AVX_256/bin
 else
-        alias mdrun="gmx mdrun"
+        PATH=$PATH:/gromacs/SSE2/bin
 fi
 
 export PATH
-
